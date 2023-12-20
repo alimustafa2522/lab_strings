f060e1c13cc1bbfebad53097f3e8b9ea70916cfd
alimustafa <alimustafa2522@gmail.com>
2023-12-21 01:21:31 +0500

    blankstring.c

--- include/gameFunctions.h
+++ include/gameFunctions.h
@@ -1,8 +1,20 @@
-
+#include<stdio.h>
 //TODO: define, document, and implement this function
-initializeBlankString();
+void initializeBlankString(int lenght, char *stirng);
 
+void initializeBlankString(int lenght, char *string)
+{
+        int i;
+        for(i=0;i<lenght;i++)
+        {
+            string[i] == '_';
+
+        }
+        char[lenght] = '\0';
+}
 //TODO: define, document, and implement this function
+
+
 printWithSpaces();
 
 //TODO: define, document, and implement this function
@@ -30,3 +42,18 @@ void clearScreen();
  * the two numbers.
  */
 void drawHorse(int guessedSoFar, int allowedGuesses);
+
+
+int main()
+{
+    int lenght;
+    printf("Enter the lenght of the string");
+    scanf("%d",&lenght);
+
+    char string[lenght + 1]; // plus 1 for null;
+
+    printf("Enter string");
+    scanf("%s",&string);
+
+    initializeBlankString(lenght,*string);
+}
